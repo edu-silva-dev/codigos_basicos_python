182080a9cece9be88342c1b55783dd8156bd31d7
edu-silva-dev <147456509+edu-silva-dev@users.noreply.github.com>
2023-10-10 10:35:41 -0300

    segunda alteração

--- tratamento_erros.py
+++ tratamento_erros.py
@@ -3,3 +3,14 @@
 #caso um número ímpar seja digitado, informe ao usuário que ele digitou um valor ímpar e peça novamente por um número par;
 
 #caso uma letra seja digitada, informe ao usuário que ele digitou um caractere inválido e peça novamente por um número par.
+
+def numerosPares(num1):
+    if num1 %2 ==0:
+        print('O número informado é par')
+    else:
+        print('O número informado é impar')
+   
+    
+ 
+
+numerosPares(7)
