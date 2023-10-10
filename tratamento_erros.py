79752be5c9f8e0d846b3ae98c3b37465c751f4fa
edu-silva-dev <147456509+edu-silva-dev@users.noreply.github.com>
2023-10-10 09:47:35 -0300

    primeira alteração

---
+++ tratamento_erros.py
@@ -0,0 +1,5 @@
+#Desenvolva um programa que só deve aceitar números pares. Siga as seguintes instruções:
+
+#caso um número ímpar seja digitado, informe ao usuário que ele digitou um valor ímpar e peça novamente por um número par;
+
+#caso uma letra seja digitada, informe ao usuário que ele digitou um caractere inválido e peça novamente por um número par.
